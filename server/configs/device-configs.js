8ca2f3633001dd362686515825144ff2548d0a48
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-07 01:26:12 -0500

    Updated device handler

--- server/configs/device-configs.js
+++ server/configs/device-configs.js
@@ -5,13 +5,34 @@ const logger = require(`${dir.utils}logger`)
 const portConfiguration = port => () => port++
 const getNextPort = portConfiguration(config.getPort())
 
-const logAction = name => action => logger.log(`${name}:`, action)
+const logDeviceState = name => state => logger.log(`${name}:`, state)
+
+const handleStateChange = (deviceName, deviceActions) => state => {
+	logDeviceState(name)(state)
+
+	fetch(`http://${deviceName}/${deviceActions[state]}`)
+	.then(response => response.text())
+	.then(text => logger.log(deviceName, text))
+}
+
+// const ON_OFF_ACTIONS = {
+// 	on: '/on',
+// 	off: '/off',
+// }
+
+const OPEN_CLOSE_ACTIONS = {
+	on: '/open',
+	off: '/close',
+}
 
 const deviceConfigs = [
 	{
 		name: 'Shark Stopper',
 		port: getNextPort(),
-		handler: logAction('Shark Stopper'),
+		handler: handleStateChange(
+			'shark-stopper',
+			OPEN_CLOSE_ACTIONS
+		),
 	},
 ]
 
