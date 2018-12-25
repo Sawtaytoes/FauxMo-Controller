9b06ccdb745e5222c22dbb30d201cbf4e3d0f2c9
Kevin Ghadyani <Saturn2888@gmail.com>
2018-12-25 02:35:37 -0600

    Added Living Room Garland

--- server/configs/device-configs.js
+++ server/configs/device-configs.js
@@ -10,30 +10,38 @@ const getNextPort = portConfiguration(config.getPort())
 const logDeviceState = name => state => logger.log(`${name}:`, state)
 
 const handleStateChange = (deviceName, deviceActions) => state => (
-	axios.request(`http://${deviceName}/${deviceActions[state]}`)
+	axios.request(`http://${deviceName}${deviceActions[state]}`)
 	.then(({ data }) => logDeviceState(deviceName)(data))
 	.catch(logger.logError)
 )
 
-// const ON_OFF_ACTIONS = {
-// 	on: 'on',
-// 	off: 'off',
-// }
-
-const OPEN_CLOSE_ACTIONS = {
-	on: 'open',
-	off: 'close',
+const LIGHTS_ACTIONS = {
+	on: '/lights/on',
+	off: '/lights/off',
 }
+
+// const OPEN_CLOSE_ACTIONS = {
+// 	on: 'open',
+// 	off: 'close',
+// }
 
 const deviceConfigs = [
 	{
-		name: 'Shark Stopper',
+		name: 'Living Room Garland',
 		port: getNextPort(),
 		handler: handleStateChange(
-			'shark-stopper',
-			OPEN_CLOSE_ACTIONS
+			'living-room-garland',
+			LIGHTS_ACTIONS
 		),
 	},
+	// {
+	// 	name: 'Shark Stopper',
+	// 	port: getNextPort(),
+	// 	handler: handleStateChange(
+	// 		'shark-stopper',
+	// 		OPEN_CLOSE_ACTIONS
+	// 	),
+	// },
 ]
 
 module.exports = deviceConfigs
